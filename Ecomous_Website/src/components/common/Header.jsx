dad37888dc5f368153b558ab81705f55323f0fd1
anshchovatiya <sqanshchovatiya800@gmail.com>
2024-09-07 07:36:31 +0530

    navbar dropdown started

--- Ecomous_Website/src/components/common/Header.jsx
+++ Ecomous_Website/src/components/common/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import logo from "../../assets/images/logo.svg";
 import styles from "./css/Header.module.css";
 import { IoIosArrowDown } from "react-icons/io";
@@ -7,32 +7,55 @@ import { FiUser } from "react-icons/fi";
 import { LuHeart } from "react-icons/lu";
 import { PiShoppingCartBold } from "react-icons/pi";
 import { CgMenuLeft } from "react-icons/cg";
+
+import { HeaderNavImages } from "../../data/images";
+import { FaAngleRight } from "react-icons/fa";
+
 const Header = () => {
+  
+  let [curNav,setCurrentNav] = useState(null);
   let Header = useRef(null);
+
+  let timeout = useRef(null);
 
   let oldScroll = window.scrollY;
   window.onscroll = function (event) {
-
-    if(window.scrollY < 400){
+    if (window.scrollY < 400) {
       Header.current.classList.remove("sticky");
-    }
-    else if (oldScroll < window.scrollY) {
+    } else if (oldScroll < window.scrollY) {
       Header.current.classList.remove("sticky");
     } else {
       Header.current.classList.add("sticky");
     }
 
-     oldScroll = window.scrollY;
+    oldScroll = window.scrollY;
   };
-  
+
+
+  function handleNavMouseOver(cur){
+    if(timeout.current){
+      clearTimeout(timeout.current);
+    }
+    setCurrentNav(cur);
+  }
+  function handleNavMouseOut(){
+    timeout.current = setTimeout(()=>{
+      setCurrentNav(null)
+    },500);
+  }
 
 
   return (
-    <div className={`${styles.headerContainer} w-screen overflow-x-hidden`} ref={Header}>
-      <span className={styles.menuIcon}><CgMenuLeft /></span>
+    <div
+      className={`${styles.headerContainer} w-screen overflow-x-hidden`}
+      ref={Header}
+    >
+      <span className={styles.menuIcon}>
+        <CgMenuLeft />
+      </span>
       <img src={logo} alt="Ecomous logo" className={styles.logo} />
       <ul className={styles.navLinks}>
-        <li className={styles.navOption}>
+        <li className={styles.navOption} onMouseOver={()=>handleNavMouseOver("home")} onMouseLeave={handleNavMouseOut}>
           <a href="#">
             <span>Home</span>
             <IoIosArrowDown />
@@ -68,16 +91,50 @@ const Header = () => {
             <IoIosArrowDown />
           </a>
         </li>
-    
       </ul>
       <div className={styles.iconContainer}>
-        <span><IoSearch /></span>
-        <span className={styles.hider}><FiUser /></span>
-        <span className={styles.hider}><LuHeart /></span>
-        <span><PiShoppingCartBold /></span>
+        <span>
+          <IoSearch />
+        </span>
+        <span className={styles.hider}>
+          <FiUser />
+        </span>
+        <span className={styles.hider}>
+          <LuHeart />
+        </span>
+        <span>
+          <PiShoppingCartBold />
+        </span>
       </div>
+
+      {curNav === "home" && <HomeDropDown handleNavMouseOver={handleNavMouseOver} handleNavMouseOut={handleNavMouseOut}/>}
     </div>
   );
 };
 
 export default Header;
+
+function HomeDropDown({handleNavMouseOver,handleNavMouseOut}) {
+
+
+  return (
+    <div onMouseEnter={()=>handleNavMouseOver("home")} onMouseLeave={()=>handleNavMouseOut()} className="hidden lg:block absolute top-full left-0 w-screen bg-white z-[1000] min-1150:px-[140px] py-[40px] NavDropDown">
+      <div className="grid grid-cols-3 min-1150:grid-cols-6 gap-x-[20px] gap-y-[25px] px-[50px]">
+        {HeaderNavImages.map((cur,index) => {
+          return (
+            <div className="border border-white px-[7px] py-[6px] shadow-[0_8px_20px_rgba(0,0,0,0.06)] rounded-sm overflow-hidden hover:border-black cursor-pointer">
+              <img src={cur.image} alt="Demo web image"></img>
+              <p className="leading-[45px] text-center text-nowrap text-[14px] font-medium">{cur.name}</p>
+            </div>
+          );
+        })}
+      </div>
+      <button className="flex items-center px-[30px] leading-[50px] bg-black text-white gap-2 rounded-[4px] m-auto mt-[48px]">
+        <span>Shop collection</span>
+        <span className="text-[18px]">
+          <FaAngleRight />
+        </span>
+      </button>
+    </div>
+  );
+}
---
+++ Ecomous_Website/src/data/Images.jsx
@@ -0,0 +1,27 @@
+import headerImage1 from "../assets/images/home-01.jpg";
+import headerImage2 from "../assets/images/home-multi-brand.jpg";
+import headerImage3 from "../assets/images/home-02.jpg";
+import headerImage4 from "../assets/images/home-03.jpg";
+import headerImage5 from "../assets/images/home-04.jpg";
+import headerImage6 from "../assets/images/home-05.jpg";
+import headerImage7 from "../assets/images/home-06.jpg";
+import headerImage8 from "../assets/images/home-07.jpg";
+import headerImage9 from "../assets/images/home-08.jpg";
+import headerImage10 from "../assets/images/home-skincare.jpg";
+import headerImage11 from "../assets/images/home-headphone.jpg";
+import headerImage12 from "../assets/images/home-giftcard.jpg";
+
+export const HeaderNavImages = [
+  { image: headerImage1, name: "Home Fashion 01" },
+  { image: headerImage2, name: "Home Fashion 02" },
+  { image: headerImage3, name: "Home Fashion 03" },
+  { image: headerImage4, name: "Home Fashion 04" },
+  { image: headerImage5, name: "Home Fashion 05" },
+  { image: headerImage6, name: "Home Fashion 06" },
+  { image: headerImage7, name: "Home Fashion 07" },
+  { image: headerImage8, name: "Home Fashion 08" },
+  { image: headerImage9, name: "Home Fashion 09" },
+  { image: headerImage10, name: "Home Skincare" },
+  { image: headerImage11, name: "Home Headphone" },
+  { image: headerImage12, name: "Home Gift Card" },
+];
