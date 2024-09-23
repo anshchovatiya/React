2b02f270b030bae45451583d750e14c1e2793994
bansi43 <bansiundhad34@gmail.com>
2024-09-23 09:02:36 +0530

    background color change on recipe app

--- recipe_app/src/app/page.js
+++ recipe_app/src/app/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-      <div className="flex items-center justify-center h-svh">
+      <div className="flex items-center justify-center h-svh bg-[black]">
            <Link href="/recipes/vegetarian" className="border border-white text-white text-sm   py-3 rounded hover:bg-white duration-500 hover:text-black px-10 h-11 font-semibold">
           Go to Recipe Page
         </Link>
